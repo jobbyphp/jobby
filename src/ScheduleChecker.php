8765a6ea65ed89e5b886ccc2d2a9b89bc705a9f1
Gareth Ellis <garethellis@gmail.com>
2016-09-05 13:58:22 +0100

    Adds ScheduleChecker class and makes use of it in BackgroundJob::shouldRun()

---
+++ src/ScheduleChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jobby;
+
+use Cron\CronExpression;
+
+class ScheduleChecker
+{
+    /**
+     * @param string $schedule
+     * @return bool
+     */
+    public function isDue($schedule)
+    {
+        $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $schedule);
+        if ($dateTime !== false) {
+            return $dateTime->format('Y-m-d H:i') == (date('Y-m-d H:i'));
+        }
+
+        return CronExpression::factory($schedule)->isDue();
+    }
+}
